77375f642c572dfbd9b2dbe41344cfbfb379e5cf
Piotrek <piotrziobrowski.work@gmail.com>
2024-03-23 20:44:32 +0100

    new fundament

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.graphics import Color, Rectangle
+
+
+class BackgroundLayout(FloatLayout):
+    def __init__(self, **kwargs):
+        super(BackgroundLayout, self).__init__(**kwargs)
+        with self.canvas.before:
+            self.bg_color = Color(0.1, 0.2, 0.3, 1)  # Kolor tła
+            self.rect = Rectangle(size=(self.width, self.height), pos=self.pos)
+
+        # Aby tło aktualizowało się razem z rozmiarem okna
+        self.bind(size=self._update_bg, pos=self._update_bg)
+
+    def _update_bg(self, *args):
+        self.rect.size = self.size
+        self.rect.pos = self.pos
+
 
 class FlashcardApp(App):
     def build(self):
-        # Window setup
-        self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.6, 0.8)
-        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        self.title = 'Flashcard App'
+        window = BackgroundLayout()
 
-        # Logo
-        self.logo = Image(source="logo.png", size_hint_y=None, height=200)
-        self.window.add_widget(self.logo)
+        # Dodawanie widgetów do BackgroundLayout
+        logo = Image(source="logo.png", size_hint=(None, None), size=(400, 200), pos_hint={'center_x': 0.5, 'top': 1})
+        window.add_widget(logo)
 
-        # Buttons
         button_texts = ["Moje fiszki", "Utwórz fiszki", "Importuj fiszki", "Statystyki"]
-        for text in button_texts:
-            button = Button(text=text, size_hint_y=None, height=70, bold=True, background_color='#00FFCE')
-            button.bind(on_press=self.callback)
-            self.window.add_widget(button)
+        for i, text in enumerate(button_texts, start=1):
+            btn = Button(text=text, size_hint=(None, None), size=(200, 50),
+                         pos_hint={'center_x': 0.5, 'center_y': 0.6 - i * 0.1})
+            window.add_widget(btn)
 
-        return self.window
+        return window
 
-    def callback(self, instance):
-        # Placeholder callback function
-        pass
 
 if __name__ == "__main__":
     FlashcardApp().run()
