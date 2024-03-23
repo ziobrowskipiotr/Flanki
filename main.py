d3b93a39f4ea765b1c147a448e06c6ecce9db4a9
Krzyhu <jkrzyszczuk@student.agh.edu.pl>
2024-03-23 20:30:39 +0100

    pierwsze zmiany

--- main.py
+++ main.py
@@ -1,56 +1,33 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.image import Image
 from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
 
-class SayHello(App):
+class FlashcardApp(App):
     def build(self):
-        #returns a window object with all it's widgets
+        # Window setup
         self.window = GridLayout()
         self.window.cols = 1
-        self.window.size_hint = (0.6, 0.7)
-        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
+        self.window.size_hint = (0.6, 0.8)
+        self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
 
-        # image widget
-        self.window.add_widget(Image(source="logo.png"))
+        # Logo
+        self.logo = Image(source="logo.png", size_hint_y=None, height=200)
+        self.window.add_widget(self.logo)
 
-        # label widget
-        self.greeting = Label(
-                        text= "What's your name?",
-                        font_size= 18,
-                        color= '#00FFCE'
-                        )
-        self.window.add_widget(self.greeting)
-
-        # text input widget
-        self.user = TextInput(
-                    multiline= False,
-                    padding_y= (20,20),
-                    size_hint= (1, 0.5)
-                    )
-
-        self.window.add_widget(self.user)
-
-        # button widget
-        self.button = Button(
-                      text= "GREET",
-                      size_hint= (1,0.5),
-                      bold= True,
-                      background_color ='#00FFCE',
-                      #remove darker overlay of background colour
-                      # background_normal = ""
-                      )
-        self.button.bind(on_press=self.callback)
-        self.window.add_widget(self.button)
+        # Buttons
+        button_texts = ["Moje fiszki", "Utwórz fiszki", "Importuj fiszki", "Statystyki"]
+        for text in button_texts:
+            button = Button(text=text, size_hint_y=None, height=70, bold=True, background_color='#00FFCE')
+            button.bind(on_press=self.callback)
+            self.window.add_widget(button)
 
         return self.window
 
     def callback(self, instance):
-        # change label text to "Hello + user name!"
-        self.greeting.text = "SIema " + self.user.text + "!"
+        # Placeholder callback function
+        pass
 
-# run Say Hello App Calss
 if __name__ == "__main__":
-    SayHello().run()
+    FlashcardApp().run()
