d53d35f4f6d27f4cb5c519edac6f3517370e5909
Piotrek <piotrziobrowski.work@gmail.com>
2024-03-30 17:44:07 +0100

    changes

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from kivy.app import App
 from kivy.graphics import Color, RoundedRectangle
 from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+
+
+class MyAccountWindow(Popup):
+    def __init__(self, **kwargs):
+        super(MyAccountWindow, self).__init__(**kwargs)
+
+        self.title = "Moje konto"
+
+        layout = BoxLayout(orientation='vertical')
+        self.content = layout
+
+        back_button = Button(text='Powrót', size_hint=(None, None), size=(100, 50))
+        back_button.bind(on_release=self.dismiss)
+        layout.add_widget(back_button)
+
+    pass
 
 
 class MyButton(Button):
@@ -19,6 +37,7 @@ class MainApp(App):
 
     def build(self):
         return Builder.load_file('flanki.kv')
+
 
 if __name__ == '__main__':
     MainApp().run()
