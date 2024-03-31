680eaa352363e9893a49f02f8f86370975ac84f4
Piotrek <piotrziobrowski.work@gmail.com>
2024-03-31 13:49:53 +0200

    New features

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from kivy.app import App
 from kivy.lang import Builder
-from MyButton import MyButton
+from kivy.uix.screenmanager import Screen
+from MyButton import MyButton  # Zaimportuj swój niestandardowy przycisk
+
+
+class MainScreen(Screen):
+    pass
+
+
+class MojeKontoScreen(Screen):
+    pass
 
 
 class MainApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
     def build(self):
+        # Załaduj najpierw definicje dla poszczególnych ekranów
+        Builder.load_file('MojeKontoScreen.kv')
+        # Następnie załaduj główny plik .kv, który korzysta z tych ekranów
         return Builder.load_file('main.kv')
+
+    def change_screen(self, screen_name):
+        if screen_name in self.root.screen_names:
+            self.root.current = screen_name
+        else:
+            print(f"Ekran '{screen_name}' nie istnieje.")
 
 
 if __name__ == '__main__':
